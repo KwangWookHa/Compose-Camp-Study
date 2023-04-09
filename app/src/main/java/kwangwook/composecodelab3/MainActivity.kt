c0bf956298ca7280ba76f005fb65a12cd9e96c06
Ha Kwang Wook <voo@fingerlabs.io>
2023-04-09 21:19:04 +0900

    5. 컴포저블 재사용 완료

--- app/src/main/java/kwangwook/composecodelab3/MainActivity.kt
+++ app/src/main/java/kwangwook/composecodelab3/MainActivity.kt
@@ -3,13 +3,17 @@ package kwangwook.composecodelab3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kwangwook.composecodelab3.ui.theme.ComposeCodelab3Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
             ComposeCodelab3Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -28,13 +32,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+            Text(text = "Hello")
+            Text(text = name)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("Bitcoin", "Ethereum")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeCodelab3Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
