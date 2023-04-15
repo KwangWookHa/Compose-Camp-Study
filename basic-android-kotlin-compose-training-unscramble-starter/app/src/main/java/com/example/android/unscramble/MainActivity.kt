0d7ea604acb99e890150f41a8ef4a98a7f568e2a
Ha Kwang Wook <kwangwookha@icloud.com>
2023-04-15 15:41:27 +0900

    GameScreen UI 설계

--- basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/MainActivity.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/MainActivity.kt
@@ -19,13 +19,17 @@ package com.example.android.unscramble
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.example.android.unscramble.ui.GameScreen
+import com.example.android.unscramble.ui.GameViewModel
 import com.example.android.unscramble.ui.theme.UnscrambleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val gameViewModel: GameViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,7 +39,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    GameScreen()
+                    GameScreen(gameViewModel = gameViewModel)
                 }
             }
         }
